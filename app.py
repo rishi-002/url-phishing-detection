e265c3de429553b63a923772e0c98be9663ee887
Rishi Kant <0210rishi@gmail.com>
2026-03-17 15:27:09 +0530

    fixed render port

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def subscribe():
     return render_template("index.html", predict="Subscribed successfully!")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
